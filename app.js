6dfc2b0928ef128543f366db7e28b49797fcd971
Didac Fernandez <didac.fernandez@insiemecloud.net>
2022-10-23 17:10:29 -0700

    Defined Start Stop interval and Display Next mini-grid

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const tTetromino = [
     [1, width, width+1, width*2+1]
 ]
 const oTetromino = [
-    [0, 1, width, width+1],
+    
     [0, 1, width, width+1],
     [0, 1, width, width+1],
     [0, 1, width, width+1]        
@@ -71,7 +71,20 @@ function undraw() {
 //Timer Interval to move tetromino down every second
 timerid = setInterval(moveDown, 1000)
 
-//Define moveDown function
+// Assign functions to arrow keys using keycodes, http://keycode.info/
+function control(e) {
+  if(e.keyCode === 37) {
+    moveLeft()
+  } else if (e.keyCode === 38) {
+    rotate()
+  } else if (e.keyCode === 39) {
+    moveRight()
+  } else if (e.keyCode === 40) {
+    moveDown()
+}
+document.addEventListener('keyup', control)
+
+//Descending Function
 function moveDown(){
     undraw()
     currentPosition += width
@@ -94,10 +107,49 @@ function freeze(){
 function moveLeft(){
     undraw()
     const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+    
     if(!isAtLeftEdge) currentPosition -=1
+    
     if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
         currentPosition -=1
     }
     draw()
 }
+
+// Move tetromino right, unless edge or blockage
+function moveRight(){
+    undraw()
+    const isAtRighEdge = current.some(index => (currentPosition + index) % width === -1)
+    if(!isAtRightEdge) currentPosition +=1
+    
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+        currentPosition -=1
+    }
+    draw()
+}
+
+// Rotate function for the tetrominoes
+function rotate() {
+    undraw()
+    currentRotation++
+    if(currentRotation === current.length) {
+        currentRotation = 0
+    }
+    current = theTetrominoes[random][currentRotation]
+    draw()
+}
+
+// Preview in mini-grid next tetromino
+const displaySquares = document.querySelectorAll('.mini-grid div')
+const displayWidth = 4
+let displayIndex = 0
+
+// Tetrominos on mini-display without rotation
+const upNextTetrominoes = [
+    [1, displayWidth+1, displayWidth*2+1, 2], //lTetromino
+    [0, displayWidth, displayWidth+1, displayWidth*2+1], //zTetromino
+    [1, displayWidth, displayWidth+1, displayWidth+2], //tTetromino
+    [0, 1, displayWidth, displayWidth+1], //oTetromino
+    [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1], //iTetromino
+]
 })
