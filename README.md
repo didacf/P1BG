b72d8f97d01968cef8a07ecbd1b288725d2caeb4
Didac Fernandez <didac.fernandez@insiemecloud.net>
2022-10-23 18:51:03 -0700

    Fixed missing bracket

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # P1BG
 This is the first milestone project, for my SDSU Development program with ThriveDX
 I am attempting to recreate the famous game Tetris with basic functionality.
+Instructions:
+
+Use the keyboard arrow keys, left to move left, Right to move Right, Down to go down and Up Arrow to rotate
+
+Thanks to Ania Kubow so I learned and practice how to do this
--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 // Define EventListener on DOM Content Load:
-
 document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid')
   let squares = Array.from(document.querySelectorAll('.grid div'))
@@ -19,7 +18,6 @@ document.addEventListener('DOMContentLoaded', () => {
 ]
     
 // Define Tetrominoes (Tetris blocks) arrays
-
 const lTetromino = [
     [1, width+1, width*2+1, 2],
     [width, width+1, width+2, width*2+2],
@@ -33,6 +31,7 @@ const zTetromino = [
     [0, width, width+1, width*2+1],
     [width+1, width+2, width*2, width*2+1]      
 ]
+
 const tTetromino = [
     [1, width, width+1, width+2],
     [1, width+1, width+2, width*2+1],
@@ -104,10 +103,9 @@ function moveDown(){
 }
 
 // Freeze functions with new tetromino falling down 
-function freeze(){
+function freeze() {
     if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
         current.forEach(index => squares[currentPosition + index].classList.add('taken'))
-
     // New tetromino falling down the screen 
     random = nextRandom
     nextRandom =   Math.floor(Math.random() * theTetrominoes.length)
@@ -134,9 +132,7 @@ function moveLeft(){
 function moveRight(){
     undraw()
     const isAtRighEdge = current.some(index => (currentPosition + index) % width === -1)
-
     if(!isAtRightEdge) currentPosition +=1
-    
     if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
         currentPosition -=1
     }
@@ -159,13 +155,13 @@ const displaySquares = document.querySelectorAll('.mini-grid div')
 const displayWidth = 4
 const displayIndex = 0
 
-// Tetrominos on mini-display without rotation //
+// Tetrominos on mini-display without rotation 
 const upNextTetrominoes = [
-    [1, displayWidth+1, displayWidth*2+1, 2], // lTetromino
-    [0, displayWidth, displayWidth+1, displayWidth*2+1], //zTetromino //
+    [1, displayWidth+1, displayWidth*2+1, 2], //lTetromino
+    [0, displayWidth, displayWidth+1, displayWidth*2+1], //zTetromino
     [1, displayWidth, displayWidth+1, displayWidth+2], //tTetromino
     [0, 1, displayWidth, displayWidth+1], //oTetromino
-    [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1], // iTetromino
+    [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1], //iTetromino
 ]
 
 //Display shape mini-grid funcion //
@@ -221,4 +217,5 @@ function gameOver() {
         clearInterval(timerId)
     }
 }
+
 })
