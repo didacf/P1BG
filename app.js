29a9eb567f6af59f4f1b94d41dca2462c93bdc8e
Didac Fernandez <didac.fernandez@insiemecloud.net>
2022-10-23 17:23:55 -0700

    Added Score function

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid')
   let squares = Array.from(document.querySelectorAll('.grid div'))
   const ScoreDisplay = document.querySelector('#score')
-  const StartBtn= document.querySelector('#start-button')
+  const startBtn= document.querySelector('#start-button')
   const width = 10
+  let nextRandom = 0
+  let timerid
     
 // Define Tetrominoes (Tetris blocks) arrays
 
@@ -69,17 +71,17 @@ function undraw() {
   } 
   
 //Timer Interval to move tetromino down every second
-timerid = setInterval(moveDown, 1000)
+//timerid = setInterval(moveDown, 1000)
 
-// Assign functions to arrow keys using keycodes, http://keycode.info/
-function control(e) {
-  if(e.keyCode === 37) {
+// Assign functions to arrow keys using keycodes, https://www.toptal.com/developers/keycode
+function control(key) {
+  if(key.keyCode === 37) {
     moveLeft()
-  } else if (e.keyCode === 38) {
+  } else if (key.keyCode === 38) {
     rotate()
-  } else if (e.keyCode === 39) {
+  } else if (key.keyCode === 39) {
     moveRight()
-  } else if (e.keyCode === 40) {
+  } else if (key.keyCode === 40) {
     moveDown()
 }
 document.addEventListener('keyup', control)
@@ -97,10 +99,12 @@ function freeze(){
     if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
         current.forEach(index => squares[currentPosition + index].classList.add('taken'))
     // New tetromino falling down the screen 
-    random =   Math.floor(Math.random() * theTetrominoes.length)
+    random = nextRandom
+    nextRandom =   Math.floor(Math.random() * theTetrominoes.length)
     current = theTetrominoes[random][currentRotation]
     currentPosition = 4
     draw()
+    displayShape()
     }
 }
 // Move tetromino left, unless edge or blockage
@@ -144,12 +148,58 @@ const displaySquares = document.querySelectorAll('.mini-grid div')
 const displayWidth = 4
 let displayIndex = 0
 
-// Tetrominos on mini-display without rotation
+
+// Tetrominos on mini-display without rotation //
 const upNextTetrominoes = [
-    [1, displayWidth+1, displayWidth*2+1, 2], //lTetromino
-    [0, displayWidth, displayWidth+1, displayWidth*2+1], //zTetromino
+    [1, displayWidth+1, displayWidth*2+1, 2], // lTetromino
+    [0, displayWidth, displayWidth+1, displayWidth*2+1], //zTetromino //
     [1, displayWidth, displayWidth+1, displayWidth+2], //tTetromino
     [0, 1, displayWidth, displayWidth+1], //oTetromino
-    [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1], //iTetromino
+    [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1], // iTetromino
 ]
+
+//Display shape mini-grid funcion //
+function displayShape() {
+    // Remove tetromino form the entire grid //
+    displaySquares.forEach(square => {
+        square.classList.remove('tetromino')
+    })
+    upNextTetrominoes[nextRandom].forEach( index => {
+        displaySquares[displayIndex + index].classList.add('tetromino')
+    })
+}
+
+//Define Start/Pause Functionality
+startBtn.addEventListener('click', () => {
+    if (timerid) {
+        clearInterval(timerId)
+        timerId = null
+    } else {
+        draw()
+        timerId = setInterval(moveDown, 1000)
+        nextRandom = Math.floor(Math.random()*theTetrominoes.length)
+        displayShape()
+    }
+})
+
+//Score counter and display definition
+function addScore() {
+    for (let i= 0; i < 199; i +=width) {
+        const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+8, i+9]
+        
+        if (row.every(index => squares[index].classList.contains('taken'))) {
+            score += 10
+            scoreDisplay.innerHTML = score
+            row.forEach(index => {
+                squares[index].classList.remove('taken')
+                squares[index].classList.remove('tetromino')
+                squares[index].style.backgroundColor = ''
+            })
+            const squaresRemoved = squares.splice(i, width)
+            squares = squaresRemoved.concat(squares)
+            squares.forEach(cell => grid.appendChild(cell))
+        }
+    }
+}
+
 })
