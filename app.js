cb6caaf1d4f30a6c588ff294af82a41b36ada507
Didac Fernandez <didac.fernandez@insiemecloud.net>
2022-10-23 18:01:42 -0700

    Fixes and last touches

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const lTetromino = [
     [1, width+1, width*2+1, width*2],
     [width+1, width*2, width*2+1,width*2+2]
 ]
+
 const zTetromino = [
     [0, width, width+1, width*2+1],
     [width+1, width+2, width*2, width*2+1],
@@ -34,12 +35,14 @@ const tTetromino = [
     [width, width+1,width+2, width*2+1],
     [1, width, width+1, width*2+1]
 ]
+
 const oTetromino = [
     
     [0, 1, width, width+1],
     [0, 1, width, width+1],
     [0, 1, width, width+1]        
 ]
+
 const iTetromino = [
     [1, width+1, width*2+1, width*3+1],
     [width, width+1, width+2, width+3],
